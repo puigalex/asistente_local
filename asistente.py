63060fca77a9f8ca9269a6d85979ac84dd5f4cd8
Alex <puig.alejandro24@gmail.com>
2023-03-16 17:21:32 -0600

    integracion de gpt3 probada en Jetson AGX

--- asistente.py
+++ asistente.py
@@ -3,7 +3,8 @@ import os
 import speech_recognition as sr
 import whisper
 from utils import write_file as wf
-
+import time
+import subprocess
 
 from datetime import datetime, timedelta
 from queue import Queue
@@ -11,7 +12,8 @@ from tempfile import NamedTemporaryFile
 from time import sleep
 import openai
 import gtts
-
+openai.api_key = ""
+import pygame
 
 class Asistente:
     def __init__(self, model, record_timeout, phrase_timeout, energy_threshold, wake_word):
@@ -21,15 +23,18 @@ class Asistente:
         self.phrase_time = None
         self.last_sample = bytes()
         self.data_queue = Queue() 
-        recorder = sr.Recognizer()
-        recorder.energy_threshold = energy_threshold
-        recorder.dynamic_energy_threshold = False
-        openai.api_key = ""
+        self.recorder = sr.Recognizer()
+        self.recorder.energy_threshold = energy_threshold
+        self.recorder.dynamic_energy_threshold = False
+        self.record_timeout = record_timeout
+        self.phrase_timeout = phrase_timeout
+        self.wake_word = wake_word
+
 
     def listen(self):
-        source = sr.Microphone(sample_rate=16000)
-        with source:
-            self.recorder.adjust_for_ambient_noise(source)
+        self.source = sr.Microphone(sample_rate=16000)
+        with self.source:
+            self.recorder.adjust_for_ambient_noise(self.source)
 
             def record_callback(_, audio:sr.AudioData) -> None:
                 """
@@ -41,7 +46,7 @@ class Asistente:
                 self.data_queue.put(data)
 
         #Se deja el microfono escuchando con ayuda de speech_recognition
-        self.recorder.listen_in_background(source, record_callback, phrase_time_limit=self.record_timeout)
+        self.recorder.listen_in_background(self.source, record_callback, phrase_time_limit=self.record_timeout)
         start = datetime.utcnow()
         while True:
             try:
@@ -51,18 +56,18 @@ class Asistente:
                 # Pull raw recorded audio from the queue.
                 if not self.data_queue.empty():
                     phrase_complete = False
-                    if phrase_time and now - phrase_time > timedelta(seconds=self.phrase_timeout):
-                        last_sample = bytes()
+                    if self.phrase_time and now - self.phrase_time > timedelta(seconds=self.phrase_timeout):
+                        self.last_sample = bytes()
                         phrase_complete = True
                     # This is the last time we received new audio data from the queue.
-                    phrase_time = now
+                    self.phrase_time = now
 
                     while not self.data_queue.empty():
                         data = self.data_queue.get()
-                        last_sample += data
+                        self.last_sample += data
 
 
-                    audio_data = sr.AudioData(last_sample, source.SAMPLE_RATE, source.SAMPLE_WIDTH)
+                    audio_data = sr.AudioData(self.last_sample, self.source.SAMPLE_RATE, self.source.SAMPLE_WIDTH)
                     wav_data = io.BytesIO(audio_data.get_wav_data())
 
                     with open(self.temp_file, 'w+b') as f:
@@ -79,9 +84,11 @@ class Asistente:
                     if self.wake_word in self.transcription[-1].lower():
                         # Se activo el asistente
                         mensaje = self.transcription[-1].lower().replace(self.wake_word, "")
-                        mensaje = "Human: " + mensaje
+                        mensaje = "Humano: " + mensaje
                         respuesta = self.call_gpt(mensaje)
                         print(respuesta)
+                        self.tts(respuesta)
+                        self.play_audio_pygame("audio.mp3")
 
 
                     # os.system('cls' if os.name=='nt' else 'clear')
@@ -103,16 +110,19 @@ class Asistente:
 
     def call_gpt(self, texto):
         # LLamada a GPT3 enviando la ultima oracion transcrita
+
+        print("Comunidando con openai", texto)
         respuesta = openai.Completion.create(
-            engine="ada",
+            engine="text-davinci-002",
             prompt=texto,
-            temperature=0.9,
+            temperature=0.5,
             max_tokens=50,
             top_p=1,
             frequency_penalty=0,
-            presence_penalty=0.6,
-            stop=["\n", " Human:", " AI:"]
+            presence_penalty=0.6
+            #stop=["\n", " Humano:", " Inteligencia Artificial:"]
         )
+        print("La respuesta fue: ", respuesta["choices"][0]["text"])
         return respuesta["choices"][0]["text"]
     
     def tts(self, texto):
@@ -122,9 +132,13 @@ class Asistente:
     
     def play_audio(self, filename):
         # Abre el archivo de audio y lo reproduce
-        os.system('mpg123 -q ' + filename)
-
-
-        
-
-
+        os.system("mpg123 " + filename)
+    
+    def play_audio_pygame(self, filename):
+        # Abre el archivo de audio y lo reproduce con pygame
+        pygame.mixer.init()
+        pygame.mixer.music.load(filename)
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            pygame.time.Clock().tick(10)
+    
